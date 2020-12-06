fe358ea5436040f9651083e0f5e0b42951e5075c
Delph <delph_teb@hotmail.com>
2020-12-06 01:49:50 -0500

    fix event listener bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,49 +1,50 @@
 $(document).ready(function() {
     var cityName = JSON.parse(localStorage.getItem("cityNameList")) || [];
 
-
-/*** Building the elements for the history ***/
-function cityHistory() {
-    $(".search-history").empty(); // To avoid repeated elements 
-    // Looping through the array of cities
-    for (var i = 0; i < cityName.length; i++) {
-      var a = $("<div>");
-      var name = $("<span>");
-
-      // Adding a class, attribute and text
-      a.addClass("city d-block bg-secondary p-2");
-      a.attr("data-name", cityName[i]);
-      name.text(cityName[i]);
-      a.append(name)
-      // Adding the new element to the HTML
-      $(".search-history").append(a);
+    /*** Functions to control the display of weather data ***/
+    function showAll() {
+        $(".current").show();
+        $(".forecast").show();
     }
-}
 
-function showAll() {
-    $(".current").show();
-    $(".forecast").show();
-}
+    function hideAll() {
+        $(".current").hide();
+        $(".forecast").hide();
+    }
 
-function hideAll() {
-    $(".current").hide();
-    $(".forecast").hide();
-}
+    /*** Building the elements for the history ***/
+    function cityHistory() {
+        $(".search-history").empty(); // To avoid repeated elements 
+        // Looping through the array of cities
+        for (var i = 0; i < cityName.length; i++) {
+            var a = $("<div>");
+            var name = $("<span>");
+            // Adding a class, attribute and text
+            a.addClass("city d-block bg-secondary p-2");
+            a.attr("data-name", cityName[i]);
+            name.addClass("city-text")
+            name.text(cityName[i]);
+            a.append(name)
+            // Adding the new element to the HTML
+            $(".search-history").append(a);
+        }
+    }
 
-/*** Calling API and getting the information */
-function buildQueryURL(city) {
-    var apiKey = "de59edcad53b52bdb1763734853a531f";    
-    var queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
-    var currentDate = moment().calendar();
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function(response){
-        console.log(response);
-    });
-   // return queryURL + $.param(queryParams);
-}
+    /*** Calling API and getting the information */
+    function buildQueryURL(city) {
+        var apiKey = "de59edcad53b52bdb1763734853a531f";    
+        var queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
+        var currentDate = moment().calendar();
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response){
+            console.log(response);
+        });
+        // return queryURL + $.param(queryParams);
+    }
 
      
 
@@ -52,10 +53,10 @@ function buildQueryURL(city) {
         showAll();
     } else hideAll();
        
-
+// Storing search item
     $("#add-city").on("click", function(event) {
         event.preventDefault();
-        var city = $("input").val().trim();
+        let city = $("input").val().trim();
         cityName.push(city);
         localStorage.setItem('cityNameList', JSON.stringify(cityName));
         showAll();
@@ -64,11 +65,13 @@ function buildQueryURL(city) {
         $("input").val("")
     });
 
- // TO DO: Broken
-    $(".search-history").on("click", "button", function(event) {
+
+ // when clicking on a city in the history
+    $(document).on("click", ".city", function(event) {
         event.preventDefault();
-        var city = $(this).text();
-        buildQueryURL(city);                       //TO DO: Add function to call API
+        let city = $(this).data('name');
+        console.log(city)
+        buildQueryURL(city);                       
         showAll();
     });
 })
